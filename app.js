16aeac203842e64eb7386802e22eae5c2f41429e
azoong <shshinkitec2@gmail.com>
2022-07-26 09:42:30 +0900

    첫 커밋

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connect = require("./schemas/index") //.schemas로 가능 /index는 생략가능
+const app = express();
+const port = 3000;
+
+
+connect();
+
+const goodsRouter = require("./routes/goods") //goods 라우터파일을 불러오는거 .은 현재 디렉토리
+const requestMiddleware = (req, res, next) =>{
+    console.log("Request URL:", req.originalUrl, " - ", new Date())
+    next();
+}
+
+app.use(express.json());
+app.use(requestMiddleware)
+
+app.use("/api", [goodsRouter])
+
+app.get("/", (req, res) => {
+    res.send("Hello World@@@@@@@@");
+})
+
+
+app.listen(port, () => {
+    console.log(port, "포트로 서버가 켜졌어요!");
+});
+
---
+++ routes/goods.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart")
+const router = express.Router();
+
+router.get("/goods/cart", async (req, res) => {
+  const carts = await Cart.find();
+  const goodsIds = carts.map((cart) => cart.goodsId)
+
+  const goods = await Goods.find({ goodsId: goodsIds})
+
+
+  res.json({
+      carts : carts.map((cart) => {
+          return {
+              quantity: cart.quantity,
+              goods: goods.find((item) => item.goodsId === cart.goodsId)
+          };
+      })
+  })
+})
+
+
+
+router.get("/", (req, res) => {
+  res.send("this is root page");
+});
+
+//상품목록 
+router.get("/goods", async (req, res) => {  //쿼리함수예시 /goods?category=drink
+  const { category } = req.query;
+
+  const goods = await Goods.find({ category });
+
+  res.json({
+    goods,
+  })
+})
+
+//상세조회 한개
+router.get("/goods/:goodsId", async (req, res) => {
+  const { goodsId } = req.params; //goodsId = req.params.goodsId;
+
+  const [detail] = await Goods.find({ goodsId: Number(goodsId) })
+
+
+  res.json({
+    detail,
+  })
+})
+
+
+//장바구니 상품 등록
+router.post("/goods/:goodsId/cart", async (req, res) =>{
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existsCarts.length){
+    return res.status(400).json({ success: false, errorMessage: "이미 장바구니에 들어있는 상품입니다."})
+  }
+
+  await Cart.create({ goodsId: Number(goodsId), quantity})
+  res.json({ success: true})
+})
+
+
+//장바구니 상품 삭제
+router.delete("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  
+  const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existsCarts.length){
+    await Cart.deleteOne({ goodsId: Number(goodsId)})
+  }
+  res.json({ success: true })
+})
+
+//장바구니 수량 수정
+router.put("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  if (quantity < 1){
+    return res.status(400).json({
+     errorMessage: "1 이상의 값만 입력할 수 있습니다.",
+    })
+    
+  }
+
+  const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (!existsCarts.length){
+    return res.status(400).json({ success: false, errorMessage: "장바구니에 해당 상품이 없습니다."})
+  }
+
+  await Cart.updateOne({ goodsId: Number(goodsId) }, { $set: { quantity } });
+  
+  res.json({ success: true })
+})
+
+  
+
+
+
+router.post("/goods", async (req, res) => {
+  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+
+  const goods = await Goods.find({ goodsId });
+  if (goods.length) {
+    return res.status(400).json({ success: false, errorMessage: "이미 있는 데이터입니다." })
+  }
+  const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
+  res.json({ goods: createdGoods });
+});
+
+module.exports = router; // 다른곳에 가져갈려면 모듈로써 지정해줘야됨 
+                         //module.exports = "함수이름"
